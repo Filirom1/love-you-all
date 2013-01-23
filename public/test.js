2cd15a797352b71b6358c82fc114d5d04830d420
Filirom1 <filirom1@gmail.com>
2013-01-23 21:32:11 +0100

    add sinon

--- public/test.js
+++ public/test.js
@@ -1,11 +1,18 @@
 describe('Who Tweets LyonJS', function(){
   assert = chai.assert;
 
+  var server;
+  before(function () { 
+    server = sinon.fakeServer.create(); 
+    server.respondWith(/twitter/, function(xhr){
+      xhr.respond(200, {"content-type": "application/json"}, JSON.stringify({results: [ {from_user: "name", text: "LyonJS is so good", profile_image_url: "http://placekitten.com/100/130"} ]}))
+    });
+  });
+
   it('should show the last tweet', function(done){
     $('button').click();
-    setTimeout(function(){
-      assert($('quote').text(), 'Tweet not present');
-      done();
-    }, 1000);
+    server.respond();
+    assert($('quote').text(), 'Tweet not present');
+    done();
   })
 });
